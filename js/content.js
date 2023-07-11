a20be3d011da06e0cd5f212450f1870e7082488c
Lucien Gaydos <darkstrings@gmx.com>
2023-07-10 23:27:52 -0400

    Text effects and fade-in for content

--- js/content.js
+++ js/content.js
@@ -1,13 +1,13 @@
-export const worksMenu = `<div class="works">
+export const worksMenu = `<div class="works fade_in">
 <ul>
-<li><a href="https://luciencode-gig-tracker.netlify.app" class="link" target="blank">Gig Tracker</a></li>
-<li><a href="https://luciencode-quiz.netlify.app/" class="link" target="blank">Quiz</a></li>
-<li><a href="https://luciencode-note-widget.netlify.app/" class="link" target="blank">Note taking app that stores notes in local storage</a></li>
+<li><a href="https://luciencode-gig-tracker.netlify.app" class="link small_link" target="blank">Gig Tracker</a></li>
+<li><a href="https://luciencode-quiz.netlify.app/" class="link small_link" target="blank">Quiz</a></li>
+<li><a href="https://luciencode-note-widget.netlify.app/" class="link small_link" target="blank">Note taking app that stores notes in local storage</a></li>
 <li><a href="https://luciencode-draw-widget.netlify.app/" class="link" target="blank">Drawing app</a></li>
-<li><a href="https://luciencode-weather-widget.netlify.app" class="link"target="blank">Weather App</a></li>
-<li><a href="https://luciencode-password-widget.netlify.app" class="link" target="blank">Password Generator</a></li>
-<li><a href="https://luciencode-halloween-timer.netlify.app" class="link" target="blank">The time left until Halloween</a></li></ul></div>`;
+<li><a href="https://luciencode-weather-widget.netlify.app" class="link small_link" target="blank">Weather App</a></li>
+<li><a href="https://luciencode-password-widget.netlify.app" class="link small_link" target="blank">Password Generator</a></li>
+<li><a href="https://luciencode-halloween-timer.netlify.app" class="link small_link" target="blank">The time left until Halloween</a></li></ul></div>`;
 
-export const aboutText = `<div class="about"><p>Hello!</p><p>My name is Lucien and I've had an associates degree in Web Development since 2005 and after a long stint working in other facets of IT (internet marketing assitance, desktop support and E-Mail server management) and most recently a guitar technician for eight years, I have decided to dedicate myself to returning to it.</p><p>I've spent some time away but the journey back has been a challenging yet rewarding path that I'll continue to walk and I look forward to doing so as part of a team.</p>`;
+export const aboutText = `<div class="about fade_in"><p>Hello!</p><p>My name is Lucien and I've had an associates degree in Web Development since 2005 and after a long stint working in other facets of IT (internet marketing assitance, desktop support and E-Mail server management) and most recently a guitar technician for eight years, I have decided to dedicate myself to returning to it.</p><p>I've spent some time away but the journey back has been a challenging yet rewarding path that I'll continue to walk and I look forward to doing so as part of a team.</p>`;
 
-export const splashPic = `<img class= "image-blur-edge" src='../../public/img/splash.png'>`;
+export const splashPic = `<img class= "image-blur-edge fade_in" src="public/img/splash.png">`;
--- js/script.js
+++ js/script.js
@@ -9,29 +9,60 @@ import { worksMenu } from "./content.js";
 import { aboutText } from "./content.js";
 import { splashPic } from "./content.js";
 
+// About content
 aboutLink.addEventListener("click", () => {
   contentDisplay.innerHTML = aboutText;
   contentDisplay.classList.remove("hidden");
   contactDisplay.classList.add("hidden");
 });
 
+// works content
 worksLink.addEventListener("click", () => {
   contentDisplay.innerHTML = worksMenu;
   contentDisplay.classList.remove("hidden");
   contactDisplay.classList.add("hidden");
 });
 
+//contact content
 contactLink.addEventListener("click", () => {
   contentDisplay.classList.add("hidden");
   contactDisplay.classList.remove("hidden");
 });
 
+// Splash pic in content onLoad
 addEventListener("load", () => {
   contentDisplay.innerHTML = splashPic;
 });
 
+//home link (brings back splash)
 homeLink.addEventListener("click", () => {
   contentDisplay.innerHTML = splashPic;
   contentDisplay.classList.remove("hidden");
   contactDisplay.classList.add("hidden");
+});
+
+// Text effect on mouseOver/mouseOut
+
+// About
+aboutLink.addEventListener("mouseover", () => {
+  aboutLink.classList.add("link_glitch");
+});
+aboutLink.addEventListener("mouseout", () => {
+  aboutLink.classList.remove("link_glitch");
+});
+
+//contact
+contactLink.addEventListener("mouseover", () => {
+  contactLink.classList.add("link_glitch");
+});
+contactLink.addEventListener("mouseout", () => {
+  contactLink.classList.remove("link_glitch");
+});
+
+//works
+worksLink.addEventListener("mouseover", () => {
+  worksLink.classList.add("link_glitch");
+});
+worksLink.addEventListener("mouseout", () => {
+  worksLink.classList.remove("link_glitch");
 });
