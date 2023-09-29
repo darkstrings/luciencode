cd317ad72a948a47462c02d1ef43b3d13c85f90a
Lucien Gaydos <darkstrings@gmx.com>
2023-09-29 00:42:34 -0400

    Updated with first react project

--- js/content.js
+++ js/content.js
@@ -1,8 +1,16 @@
 export const worksMenu = `<div class="works fade_in">
-<p class="text_space"> Below, you'll find some projects I have ready to show. Repositories to see the code for all of this can be found on my github.</a> Coming soon will be a banking simulator app in Javascript and a personal microblog based on MongoDB to post little updates as I go instead of updating a file each time. I have also started learning React so there will be projects using that here soon as well. Also on my list of languages to get current and refreshed on are MySQL and PHP. Stay tuned!</p>
+<p class="text_space"> Below, you'll find some projects I have ready to show. Repositories to see the code for all of this can be found on my github.</p> 
+<p>I'm currently working on react but on my list of languages to get current and refreshed on are MySQL and PHP. Stay tuned!</p>
+
+<h2>Newest Examples</h2>
 
 <ul>
+<li><a href="https://luciencode-salesdashboard.netlify.app" class="link small_link" target="blank">Sales Dashboard. My first app made in React as I continue to improve. It calculates an hourly wage, commission rate, the number of hours worked and calculates totals. You can also add sales objects that add to your total via the commission rate.</a></li>
 <li><a href="https://luciencode-gig-tracker.netlify.app" class="link small_link" target="blank">Gig Tracker. An app that musicians can use to document their performances.</a></li>
+</ul>
+
+<h2>Earlier Examples</h2>
+<ul>
 <li><a href="https://luciencodeweather.netlify.app" class="link small_link" target="blank">Weather app with an emphasis on current alerts and advisories</a></li>
 <li><a href="https://luciencode-quiz.netlify.app/" class="link small_link" target="blank">Quiz</a></li>
 <li><a href="https://luciencode-note-widget.netlify.app/" class="link small_link" target="blank">Note taking app that stores notes in local storage</a></li>
