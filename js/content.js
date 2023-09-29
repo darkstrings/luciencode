4bcbef46135f3542b748431a5d20f673c176a616
Lucien Gaydos <darkstrings@gmx.com>
2023-09-29 01:05:57 -0400

    adjust verbiage and styles

--- js/content.js
+++ js/content.js
@@ -1,20 +1,27 @@
 export const worksMenu = `<div class="works fade_in">
-<p class="text_space"> Below, you'll find some projects I have ready to show. Repositories to see the code for all of this can be found on my github.</p> 
-<p>I'm currently working on react but on my list of languages to get current and refreshed on are MySQL and PHP. Stay tuned!</p>
+<div class="works-top">
+<p class="text_space"> Below, you will find some projects that I have ready to show as I continue to work and learn more. Repositories to see the code for all of these can be found on my github.</p> 
+<p>I am currently working with React but on my list of languages to get current and refreshed on are MySQL and PHP. Stay tuned!</p>
+</div>
 
 <h2>Newest Examples</h2>
 
 <ul>
-<li><a href="https://luciencode-salesdashboard.netlify.app" class="link small_link" target="blank">Sales Dashboard. My first app made in React as I continue to improve. It calculates an hourly wage, commission rate, the number of hours worked and calculates totals. You can also add sales objects that add to your total via the commission rate.</a></li>
+<li><a href="https://luciencode-salesdashboard.netlify.app" class="link small_link" target="blank">Sales Dashboard. My first app made in React and a little bit of bootstrap as I continue to improve. It calculates an hourly wage, commission rate, the number of hours worked and calculates totals. You can also add sales objects that add to your total via the commission rate while also calculating the amount of the commission if you had to share the spoils with another salesperson.</a></li>
+
 <li><a href="https://luciencode-gig-tracker.netlify.app" class="link small_link" target="blank">Gig Tracker. An app that musicians can use to document their performances.</a></li>
 </ul>
 
 <h2>Earlier Examples</h2>
 <ul>
 <li><a href="https://luciencodeweather.netlify.app" class="link small_link" target="blank">Weather app with an emphasis on current alerts and advisories</a></li>
+
 <li><a href="https://luciencode-quiz.netlify.app/" class="link small_link" target="blank">Quiz</a></li>
+
 <li><a href="https://luciencode-note-widget.netlify.app/" class="link small_link" target="blank">Note taking app that stores notes in local storage</a></li>
+
 <li><a href="https://luciencode-password-widget.netlify.app" class="link small_link" target="blank">Password Generator</a></li>
+
 <li><a href="https://luciencode-halloween-timer.netlify.app" class="link small_link" target="blank">The time left until Halloween</a></li></ul></div>`;
 
 export const aboutText = `<div class="about fade_in"><p class="text_space">Hello!</p><p class="text_space">I'm Lucien.</p> <p class="text_space">I've had an associates degree in Web Development since 2005 and spent time in that and other facets of IT such as internet marketing assitance, desktop support and E-Mail server management as well.</p> <p class="text_space">I have most recently been a guitar technician in the retail world for the past eight years (which has kept my problem solving sharp) but I have decided to commit to returning to web coding.</p><p>I've spent some time away but the journey back has been a challenging yet fun and rewarding path that I'll continue to walk and I look forward to doing so as part of a team where I'm sure my skills and confidence will then grow exponentially as that becomes my full-time role once again.</p>`;
