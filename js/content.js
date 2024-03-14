ea57c83b28afeb8f91967c0bc4839336955cb128
Lucien Gaydos <darkstrings@gmx.com>
2024-03-14 17:18:20 -0400

    More performance tweaks

--- js/content.js
+++ js/content.js
@@ -1,33 +1,37 @@
 export const worksMenu = `<div class="works fade_in">
-<header class="works-top">
-    <p class="text_space"> Below, you will find some projects that I have ready to show as I continue to work and learn more. Repositories to see the code for all of these can be found on my github.</p> 
-    <p>I am currently working with React but on my list of languages to get current and refreshed on are MySQL and PHP. Stay tuned!</p>
+<header class="text_space">
+    <p class="top-p"> Below, you will find some projects that I have ready to show as I continue to work and learn more. Repositories to see the code for all of these can be found on my github.</p> 
+
 </header>
 
+<section class="section-newest">
 <h2>Newest Examples</h2>
+<ul class="examples-list">
+<li><a href="https://atomicoffee.netlify.app/" class="link link-works target="blank">Ever want to get yourself a plutonium powered coffee grinder? AtomiCoffee has you covered.</a></li>
 
-<ul>
-<li><a href="https://luciencode-salesdashboard.netlify.app" class="link small_link" target="blank">Sales Dashboard: <strong>(Now with useReducer!)</strong>
+<li><a href="https://luciencode-salesdashboard.netlify.app" class="link link-works target="blank">Sales Dashboard:
 My first app made in React and a little bit of bootstrap as I continue to improve. It calculates an hourly wage, commission rate, the number of hours worked and calculates totals. You can also add sales objects that add to your total via the commission rate while also calculating the amount of the commission if you had to share the spoils with another salesperson.</a></li>
+<li><a href="https://luciencode-gig-tracker.netlify.app" class="link link-works target="blank">Gig Tracker: An app that musicians can use to document their performances.</a></li>
+</section>
 
-<li><a href="https://luciencode-gig-tracker.netlify.app" class="link small_link" target="blank">Gig Tracker: An app that musicians can use to document their performances.</a></li>
 </ul>
-<section><h2>Earlier Examples</h2>
-    <ul>
-        <li><a href="https://luciencodeweather.netlify.app" class="link small_link" target="blank">Weather app with an emphasis on current alerts and advisories</a></li>
-        <li><a href="https://luciencode-quiz.netlify.app/" class="link small_link" target="blank">Quiz</a></li>
-        <li><a href="https://luciencode-note-widget.netlify.app/" class="link small_link" target="blank">Note taking app that stores notes in local storage</a></li>
-        <li><a href="https://luciencode-password-widget.netlify.app" class="link small_link" target="blank">Password Generator</a></li>
-        <li><a href="https://luciencode-halloween-timer.netlify.app" class="link small_link" target="blank">The time left until Halloween</a></li>
+<section class="section-earlier">
+<h2>Earlier Examples</h2>
+    <ul class="examples-list">
+        <li><a href="https://luciencodeweather.netlify.app" class="link link-works target="blank">Weather app with an emphasis on current alerts and advisories</a></li>
+        <li><a href="https://luciencode-quiz.netlify.app/" class="link target="blank">Quiz</a></li>
+        <li><a href="https://luciencode-note-widget.netlify.app/" class="link link-works target="blank">Note taking app that stores notes in local storage</a></li>
+        <li><a href="https://luciencode-password-widget.netlify.app" class="link link-works target="blank">Password Generator</a></li>
+        <li><a href="https://luciencode-halloween-timer.netlify.app" class="link link-works target="blank">The time left until Halloween</a></li>
     </ul>
 </section>
 
 </div>`;
 
-export const aboutText = `<article class="about fade_in"><p class="text_space">Hello!</p><p class="text_space">My name is Lucien.</p> <p class="text_space">I've had an associates degree in Web Development since 2005 and spent time in that and other facets of IT such as internet marketing assitance, desktop support and E-Mail server management as well.</p> 
+export const aboutText = `<section class="about fade_in"><p class="about-p">Hello!</p><p class="about-p">My name is Lucien.</p> <p class="about-p">I've had an associates degree in Web Development since 2005 and spent time in that and other facets of IT such as internet marketing assitance, desktop support and E-Mail server management as well.</p> 
 
-<p class="text_space">I have most recently been a guitar technician in the retail world for the past eight years (which has kept my problem solving sharp) but I have decided to commit to returning to web coding.</p>
+<p class="about-p">I have most recently been a guitar technician in the retail world for the past eight years (which has kept my problem solving sharp) but I have decided to commit to returning to web coding.</p>
 
-<p>I've spent some time away but the journey back has been a challenging yet fun and rewarding path that I'll continue to walk and I look forward to doing so as part of a team where I'm sure my skills and confidence will then grow exponentially once I get started in full-time role.</p></article>`;
+<p class="about-p">I've spent some time away but the journey back has been a challenging yet fun and rewarding path that I'll continue to walk and I look forward to doing so as part of a team where I'm sure my skills and confidence will then grow exponentially once I get started in full-time role.</p></section>`;
 
 export const splashPic = `<img class= "image-blur-edge fade_in" src="public/img/splash.webp">`;
