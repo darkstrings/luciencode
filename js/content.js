ee6672a246651b03c2857843409fa35cfdef5ee2
Lucien Gaydos <darkstrings@gmx.com>
2024-04-22 22:11:52 -0400

    Improved css

--- js/content.js
+++ js/content.js
@@ -7,22 +7,22 @@ export const worksMenu = `<div class="works fade_in">
 <section class="section-newest">
 <h2>Newest Examples</h2>
 <ul class="examples-list">
-<li><a href="https://atomicoffee.netlify.app/" class="link link-works target="blank">Ever want to get yourself a plutonium powered coffee grinder? AtomiCoffee has you covered.</a></li>
+<li><a href="https://atomicoffee.netlify.app/" class="link link-works" target="blank">Ever want to get yourself a plutonium powered coffee grinder? AtomiCoffee has you covered.</a></li>
 
-<li><a href="https://luciencode-salesdashboard.netlify.app" class="link link-works target="blank">Sales Dashboard:
+<li><a href="https://luciencode-salesdashboard.netlify.app" class="link link-works" target="blank">Sales Dashboard:
 My first app made in React and a little bit of bootstrap as I continue to improve. It calculates an hourly wage, commission rate, the number of hours worked and calculates totals. You can also add sales objects that add to your total via the commission rate while also calculating the amount of the commission if you had to share the spoils with another salesperson.</a></li>
-<li><a href="https://luciencode-gig-tracker.netlify.app" class="link link-works target="blank">Gig Tracker: An app that musicians can use to document their performances.</a></li>
+<li><a href="https://luciencode-gig-tracker.netlify.app" class="link link-works" target="blank">Gig Tracker: An app that musicians can use to document their performances.</a></li>
 </section>
 
 </ul>
 <section class="section-earlier">
 <h2>Earlier Examples</h2>
     <ul class="examples-list">
-        <li><a href="https://luciencodeweather.netlify.app" class="link link-works target="blank">Weather app with an emphasis on current alerts and advisories</a></li>
+        <li><a href="https://luciencodeweather.netlify.app" class="link link-works" target="blank">Weather app with an emphasis on current alerts and advisories</a></li>
         <li><a href="https://luciencode-quiz.netlify.app/" class="link target="blank">Quiz</a></li>
         <li><a href="https://luciencode-note-widget.netlify.app/" class="link link-works target="blank">Note taking app that stores notes in local storage</a></li>
-        <li><a href="https://luciencode-password-widget.netlify.app" class="link link-works target="blank">Password Generator</a></li>
-        <li><a href="https://luciencode-halloween-timer.netlify.app" class="link link-works target="blank">The time left until Halloween</a></li>
+        <li><a href="https://luciencode-password-widget.netlify.app" class="link link-works" target="blank">Password Generator</a></li>
+        <li><a href="https://luciencode-halloween-timer.netlify.app" class="link link-works" target="blank">The time left until Halloween</a></li>
     </ul>
 </section>
 
