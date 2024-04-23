b848286e627cb57d8a5a881caa8ebc0c359430b3
Lucien Gaydos <darkstrings@gmx.com>
2024-04-22 23:22:07 -0400

    More css tweaks

--- js/content.js
+++ js/content.js
@@ -5,23 +5,27 @@ export const worksMenu = `<div class="works fade_in">
 </header>
 
 <section class="section-newest">
-<h2>Newest Examples</h2>
+<h2 class="example-header">Newest Examples</h2>
 <ul class="examples-list">
-<li><a href="https://atomicoffee.netlify.app/" class="link link-works" target="blank">Ever want to get yourself a plutonium powered coffee grinder? AtomiCoffee has you covered.</a></li>
+<li>Ever want to get yourself a plutonium powered coffee grinder? <a href="https://atomicoffee.netlify.app/" class="link link-works" target="blank">AtomiCoffee</a> has you covered.</li>
 
-<li><a href="https://luciencode-salesdashboard.netlify.app" class="link link-works" target="blank">Sales Dashboard:
-My first app made in React and a little bit of bootstrap as I continue to improve. It calculates an hourly wage, commission rate, the number of hours worked and calculates totals. You can also add sales objects that add to your total via the commission rate while also calculating the amount of the commission if you had to share the spoils with another salesperson.</a></li>
-<li><a href="https://luciencode-gig-tracker.netlify.app" class="link link-works" target="blank">Gig Tracker: An app that musicians can use to document their performances.</a></li>
-</section>
+<li><a href="https://luciencode-salesdashboard.netlify.app" class="link link-works" target="blank">Sales Dashboard:</a>
+My first app made in React and a little bit of bootstrap as I continue to improve. It calculates an hourly wage, commission rate, the number of hours worked and calculates totals. You can also add sales objects that add to your total via the commission rate while also calculating the amount of the commission if you had to share the spoils with another salesperson.</li>
 
+<li><a href="https://luciencode-gig-tracker.netlify.app" class="link link-works" target="blank">Gig Tracker:</a> An app that musicians can use to document their performances.</li>
 </ul>
+</section>
 <section class="section-earlier">
-<h2>Earlier Examples</h2>
+<h2 class="example-header">Earlier Examples</h2>
     <ul class="examples-list">
-        <li><a href="https://luciencodeweather.netlify.app" class="link link-works" target="blank">Weather app with an emphasis on current alerts and advisories</a></li>
+        <li><a href="https://luciencodeweather.netlify.app" class="link link-works" target="blank">Weather app</a> with an emphasis on current alerts and advisories</li>
+
         <li><a href="https://luciencode-quiz.netlify.app/" class="link target="blank">Quiz</a></li>
+
         <li><a href="https://luciencode-note-widget.netlify.app/" class="link link-works target="blank">Note taking app that stores notes in local storage</a></li>
+
         <li><a href="https://luciencode-password-widget.netlify.app" class="link link-works" target="blank">Password Generator</a></li>
+        
         <li><a href="https://luciencode-halloween-timer.netlify.app" class="link link-works" target="blank">The time left until Halloween</a></li>
     </ul>
 </section>
