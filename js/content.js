962f58b8210e1c8967b80b369462923930ed2333
Lucien Gaydos <darkstrings@gmx.com>
2024-08-02 16:29:12 -0400

    Put my name at the top instead of being too artsy about it. Various tweaks.

--- js/content.js
+++ js/content.js
@@ -1,6 +1,6 @@
 export const worksMenu = `<div class="works fade_in">
 <header class="text_space">
-    <p class="top-p"> Below, you will find some projects that I have ready to show as I continue to work and learn more. Repositories to see the code for all of these can be found on my github.</p> 
+    <p class="top-p"> Below, you will find some projects that I have ready to show as I continue to work on more projects to add to the site. Currently I'm working on a project that involves React Router, React Query, Context API, React Hook Form and many custom hooks. Repositories to see the code for all of these can be found on my github.</p> 
 
 </header>
 
@@ -34,8 +34,9 @@ My first app made in React and a little bit of bootstrap as I continue to improv
 
 export const aboutText = `<section class="about fade_in"><p class="about-p">Hello!</p><p class="about-p">My name is Lucien.</p> <p class="about-p">I've had an associates degree in Web Development since 2005 and spent time in that and other facets of IT such as internet marketing assitance, desktop support and E-Mail server management as well.</p> 
 
-<p class="about-p">I have most recently been a guitar technician in the retail world for the past eight years (which has kept my problem solving sharp) but I have decided to commit to returning to web coding.</p>
+<p class="about-p">I have most recently been a guitar technician in the retail world for the past eight years (which has kept my problem solving sharp) but with all locations of that company closing, I have decided to commit to returning to web development.</p>
 
-<p class="about-p">I've spent some time away but the journey back has been a challenging yet fun and rewarding path that I'll continue to walk and I look forward to doing so as part of a team where I'm sure my skills and confidence will then grow exponentially once I get started in full-time role.</p></section>`;
+<p class="about-p">In the "Works" section, you'll find examples of my work. If you want to reach out, please teel free to use the contact link. Thank you for your time!</p>
+<p class="about-p"> -Lucien</p></section>`;
 
 export const splashPic = `<img class= "image-blur-edge fade_in" src="public/img/splash.webp">`;
