90602d0d19e60aeb3ad61cae1320a394c4aa8b90
Lucien Gaydos <darkstrings@gmx.com>
2024-11-01 18:36:15 -0400

    update

--- js/content.js
+++ js/content.js
@@ -1,12 +1,16 @@
 export const worksMenu = `<div class="works fade_in">
 <header class="text_space">
-    <p class="top-p"> Below, you will find some projects that I have ready to show as I continue to work on more projects to add to the site. Repositories to see the code for all of these can be found on my github.</p> 
+    <p class="top-p">Below, you will find some projects that I have ready to show as I continue to work on more projects to add to the site. Repositories to see the code for all of these can be found on my github.</p> 
+    <br>
+    <p class="top-p"><strong>Some of these apps rely on free subscrptions of MongoDB, render.com and netlify. I give them activity weekly so that they don't get put into a sleep state but things can always change. A few may need 30 seconds to "spin up" if there's been inactivity for more than 15-30 minutes. It's not a glitch but merely a one time delay as that service wakes up if it's been inactive.</strong></p> 
 
 </header>
 
 <section class="section-newest">
 <h2 class="example-header">Newest Examples</h2>
 <ul class="examples-list">
+
+<li><a href="https://tune-shed.onrender.com/" class="link link-works" target="blank">Tune Shed: </a>An online commerce site made with the MERN stack for buying new and used guitars (Please allow for a 30 second spin-up upon first arrival)</li>
 
 <li>The employee frontend for a fictional hotel called <a href="https://harder-rock-hotel.netlify.app/" class="link link-works" target="blank"> "Harder Rock Hotel"</a> with themed cabins. It uses React, React Router, React Query, Context API, React Hook Form and many custom hooks.</li>
 
@@ -22,10 +26,6 @@ My first app made in React and a little bit of bootstrap as I continue to improv
 <h2 class="example-header">Earlier Examples</h2>
     <ul class="examples-list">
         <li><a href="https://luciencodeweather.netlify.app" class="link link-works" target="blank">Weather app</a> with an emphasis on current alerts and advisories</li>
-
-        <li><a href="https://luciencode-quiz.netlify.app/" class="link link-works target="blank">Quiz</a></li>
-
-        <li><a href="https://luciencode-note-widget.netlify.app/" class="link link-works target="blank">Note taking app that stores notes in local storage</a></li>
 
         <li><a href="https://luciencode-password-widget.netlify.app" class="link link-works" target="blank">Password Generator</a></li>
         
