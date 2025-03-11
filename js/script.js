6075d17f4719dd231eafc7d1fb136715694bf569
Lucien Gaydos <darkstrings@gmx.com>
2025-03-11 00:33:29 -0400

    Css and scripting tweaks

--- js/script.js
+++ js/script.js
@@ -1,28 +1,15 @@
-const carousel = document.querySelector(".auto__car");
-const inputs = carousel.querySelectorAll("input");
+const link = document.querySelector("navbar__list__item");
 
-const nextButton = document.getElementById("next-btn");
-nextButton.addEventListener("click", nextSlide);
+document.getElementById("contact-btn").onmouseover = function () {
+  this.classList.add("glitch");
+};
+document.getElementById("github-btn").onmouseover = function () {
+  this.classList.add("glitch");
+};
 
-const prevButton = document.getElementById("prev-btn");
-prevButton.addEventListener("click", prevSlide);
-
-let currentIndex = 0;
-
-// let touchStartX = 0;
-// let touchEndX = 0;
-
-function changeSlide() {
-  inputs[currentIndex].checked = false;
-  currentIndex = (currentIndex + 1) % inputs.length;
-  inputs[currentIndex].checked = true;
-}
-
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + inputs.length) % inputs.length;
-  inputs[currentIndex].checked = true;
-}
-
-function nextSlide() {
-  changeSlide();
-}
+document.getElementById("contact-btn").onmouseout = function () {
+  this.classList.remove("glitch");
+};
+document.getElementById("github-btn").onmouseout = function () {
+  this.classList.remove("glitch");
+};
